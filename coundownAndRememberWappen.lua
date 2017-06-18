ef36b875cbf47bb817a2765011dc9d719b81a905
Kai Eckert <eckert@hdm-stuttgart.de>
2017-06-18 04:16:26 +0200

    font angepasst

--- coundownAndRememberWappen.lua
+++ coundownAndRememberWappen.lua
@@ -3,12 +3,15 @@ local coundownAndRememberWappen = {}
 modulState = false
 
 local function load()
-  
+
   playingAreaWidth = love.graphics.getWidth()
   playingAreaHeight = love.graphics.getHeight()
-  
-  
- images = {}
+
+  font = love.graphics.newFont("fonts/carbon.ttf", 70)
+  love.graphics.setFont(font)
+
+
+  images = {}
   table.insert(images, love.graphics.newImage("crest/105px-Wappen-stuttgart-vaihingen-stadtbezirk.png"))
   table.insert(images, love.graphics.newImage("crest/107px-Wappen_Stuttgart-Hofen_svg.png"))
   table.insert(images,love.graphics.newImage("crest/111px-Wappen-stuttgart-zazenhausen.png"))
@@ -22,51 +25,51 @@ local function load()
   table.insert(images,love.graphics.newImage("crest/113px-Wappen-stuttgart-rotenberg.png"))
   table.insert(images, love.graphics.newImage("crest/114px-Wappen-stuttgart-degerloch.png"))
   table.insert(images,love.graphics.newImage("crest/117px-Coat_of_arms_of_Stuttgart_svg.png"))
-  
-  
+
+
   imageX = playingAreaWidth/5
   imageY = playingAreaHeight/#images
   remaining_time = 10
-  
+
   modulState = false
 end
 
 
 local function update(dt)
-  
+
   remaining_time = remaining_time -dt
-  
+
   if remaining_time <= 0 then
     modulState = true
-    end
-  
+  end
+
 end
 
 
 local function draw()
   for i,v in ipairs(images) do
-    
+
     if i == 1 then
       love.graphics.draw(v, playingAreaWidth/2, 25, 0, 1, 1)
     end
-    
+
     if i>1 and i <6 then
       love.graphics.draw(v, imageX *(i-1), playingAreaHeight/4, 0, 1, 1)
-      
-  end
-  if i>5 and i <10 then
-      love.graphics.draw(v, imageX *(i-5), playingAreaHeight/2, 0, 1, 1)
-      
-  end
-  
-  if i>9 and i <14 then
-      love.graphics.draw(v, imageX *(i-9), playingAreaHeight/1.3, 0, 1, 1)
-      
-  end
-  
-end
 
-love.graphics.print(math.floor(remaining_time), playingAreaWidth/2+50, playingAreaHeight-200)
+    end
+    if i>5 and i <10 then
+      love.graphics.draw(v, imageX *(i-5), playingAreaHeight/2, 0, 1, 1)
+
+    end
+
+    if i>9 and i <14 then
+      love.graphics.draw(v, imageX *(i-9), playingAreaHeight/1.3, 0, 1, 1)
+
+    end
+
+  end
+
+  love.graphics.print(math.floor(remaining_time), 40, playingAreaHeight/2)
 end
 
 local function isDone()
